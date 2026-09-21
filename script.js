ac3fadb9ea9994db9b3132cd8ef1dadb8f6d8448
Aditya Kushwah <adityakushwah1516@gmail.com>
2026-09-21 22:46:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,368 @@
+/*
+  SPICEJET VIRTUAL
+  Route Database
+*/
+
+const routes = [
+
+  ["SG252","Mumbai","Delhi","B737 MAX 8","Passenger","02:10","Trainee"],
+  ["SG476","Delhi","Mumbai","B737 MAX 8","Passenger","02:10","Trainee"],
+  ["SG495","Mumbai","Dubai","B737 MAX 8","Passenger","03:05","Trainee"],
+  ["SG988","Dubai","Mumbai","B737 MAX 8","Passenger","03:05","Trainee"],
+  ["SG232","Mumbai","Chennai","B737 MAX 8","Passenger","01:55","Trainee"],
+  ["SG976","Chennai","Mumbai","B737 MAX 8","Passenger","01:55","Trainee"],
+
+  ["SG152","Mumbai","Goa","Q400","Passenger","01:10","Cadet"],
+  ["SG996","Goa","Mumbai","Q400","Passenger","01:10","Cadet"],
+
+  ["SG499","Mumbai","Abu Dhabi","B737 MAX 8","Passenger","03:20","Trainee"],
+  ["SG969","Abu Dhabi","Mumbai","B737 MAX 8","Passenger","03:20","Trainee"],
+
+  ["SG182","Mumbai","Kolkata","B737 MAX 8","Passenger","02:45","Trainee"],
+  ["SG982","Kolkata","Mumbai","B737 MAX 8","Passenger","02:45","Trainee"],
+
+  ["SG675","Delhi","Bangalore","B737 MAX 8","Passenger","02:40","Trainee"],
+  ["SG965","Bangalore","Delhi","B737 MAX 8","Passenger","02:40","Trainee"],
+
+  ["SG271","Bangalore","Kolkata","B737 MAX 8","Passenger","02:30","Trainee"],
+  ["SG973","Kolkata","Bangalore","B737 MAX 8","Passenger","02:30","Trainee"],
+
+  ["SG355","Bangalore","Pune","Q400","Passenger","01:20","Cadet"],
+  ["SG991","Pune","Bangalore","Q400","Passenger","01:20","Cadet"],
+
+  ["SG791","Delhi","Kolkata","B737 MAX 8","Passenger","02:05","Trainee"],
+  ["SG984","Kolkata","Delhi","B737 MAX 8","Passenger","02:05","Trainee"],
+
+  ["SG209","Kolkata","Chennai","B737 MAX 8","Passenger","02:15","Trainee"],
+  ["SG972","Chennai","Kolkata","B737 MAX 8","Passenger","02:15","Trainee"],
+
+  ["SG514","Delhi","Chennai","B737-900ER","Passenger","02:45","Captain"],
+  ["SG987","Chennai","Delhi","B737-900ER","Passenger","02:45","Captain"],
+
+  ["SG178","Chennai","Bangalore","Q400","Passenger","01:00","Cadet"],
+  ["SG999","Bangalore","Chennai","Q400","Passenger","01:00","Cadet"],
+
+  ["SG567","Delhi","Dubai","B737 MAX 8","Passenger","03:40","Trainee"],
+  ["SG968","Dubai","Delhi","B737 MAX 8","Passenger","03:40","Trainee"],
+
+  ["SG832","Pune","Dubai","B737 MAX 8","Passenger","03:30","Trainee"],
+  ["SG979","Dubai","Pune","B737 MAX 8","Passenger","03:30","Trainee"],
+
+  ["SG305","Bangalore","Dubai","B737 MAX 8","Passenger","04:10","Trainee"],
+  ["SG964","Dubai","Bangalore","B737 MAX 8","Passenger","04:10","Trainee"],
+
+  ["SG582","Kolkata","Dubai","B737-900ER","Passenger","05:15","Captain"],
+  ["SG986","Dubai","Kolkata","B737-900ER","Passenger","05:15","Captain"],
+
+  ["SG320","Chennai","Dubai","B737-900ER","Passenger","04:30","Captain"],
+  ["SG975","Dubai","Chennai","B737-900ER","Passenger","04:30","Captain"],
+
+  ["SG318","Bangalore","Jaipur","B737 MAX 8","Passenger","02:35","Trainee"],
+  ["SG994","Jaipur","Bangalore","B737 MAX 8","Passenger","02:35","Trainee"],
+
+  ["SG171","Chennai","Jaipur","B737 MAX 8","Passenger","03:00","Trainee"],
+  ["SG967","Jaipur","Chennai","B737 MAX 8","Passenger","03:00","Trainee"],
+
+  ["SG409","Delhi","Najaf","B737-900ER","Passenger","04:10","Captain"],
+  ["SG981","Najaf","Delhi","B737-900ER","Passenger","04:10","Captain"],
+
+  ["SG156","Mumbai","Najaf","B737 MAX 8","Passenger","03:50","Trainee"],
+  ["SG963","Najaf","Mumbai","B737 MAX 8","Passenger","03:50","Trainee"],
+
+  ["SG173","Jaipur","Ahmedabad","Q400","Passenger","01:10","Cadet"],
+  ["SG992","Ahmedabad","Jaipur","Q400","Passenger","01:10","Cadet"],
+
+  ["SG678","Bangalore","Ahmedabad","B737 MAX 8","Passenger","02:10","Trainee"],
+  ["SG971","Ahmedabad","Bangalore","B737 MAX 8","Passenger","02:10","Trainee"],
+
+  ["SG591","Mumbai","Lucknow","B737 MAX 8","Passenger","02:00","Trainee"],
+  ["SG983","Lucknow","Mumbai","B737 MAX 8","Passenger","02:00","Trainee"],
+
+  ["SG443","Chennai","Lucknow","B737 MAX 8","Passenger","02:50","Trainee"],
+  ["SG997","Lucknow","Chennai","B737 MAX 8","Passenger","02:50","Trainee"],
+
+  ["SG188","Chennai","Cochin","Q400","Passenger","01:05","Cadet"],
+  ["SG989","Cochin","Chennai","Q400","Passenger","01:05","Cadet"],
+
+  ["SG187","Bangalore","Goa","Q400","Passenger","01:15","Cadet"],
+  ["SG966","Goa","Bangalore","Q400","Passenger","01:15","Cadet"],
+
+  ["SG382","Delhi","Leh","B737 MAX 8","Passenger","01:20","Trainee"],
+  ["SG978","Leh","Delhi","B737 MAX 8","Passenger","01:20","Trainee"],
+
+  ["SG519","Kolkata","Kathmandu","Q400","Passenger","01:30","Cadet"],
+  ["SG993","Kathmandu","Kolkata","Q400","Passenger","01:30","Cadet"],
+
+  ["SG680","Delhi","Port Blair","B737 MAX 8","Passenger","04:15","Trainee"],
+  ["SG962","Port Blair","Delhi","B737 MAX 8","Passenger","04:15","Trainee"],
+
+  ["SG446","Delhi","Sharjah","B737 MAX 8","Passenger","03:35","Trainee"],
+  ["SG985","Sharjah","Delhi","B737 MAX 8","Passenger","03:35","Trainee"],
+
+  ["SG447","Mumbai","Sharjah","B737 MAX 8","Passenger","03:10","Trainee"],
+  ["SG961","Sharjah","Mumbai","B737 MAX 8","Passenger","03:10","Trainee"],
+
+  ["SG349","Delhi","Phuket","B737-900ER","Passenger","04:20","Captain"],
+  ["SG974","Phuket","Delhi","B737-900ER","Passenger","04:20","Captain"],
+
+  ["SG431","Mumbai","Phuket","B737-900ER","Passenger","04:45","Captain"],
+  ["SG998","Phuket","Mumbai","B737-900ER","Passenger","04:45","Captain"],
+
+  ["SG214","Delhi","Ahmedabad","B737 MAX 8","Passenger","01:35","Trainee"],
+  ["SG623","Ahmedabad","Delhi","B737 MAX 8","Passenger","01:35","Trainee"],
+
+  ["SG731","Mumbai","Bangalore","B737 MAX 8","Passenger","01:50","Trainee"],
+  ["SG842","Bangalore","Mumbai","B737 MAX 8","Passenger","01:50","Trainee"],
+
+  ["SG264","Delhi","Hyderabad","B737 MAX 8","Passenger","02:05","Trainee"],
+  ["SG854","Hyderabad","Delhi","B737 MAX 8","Passenger","02:05","Trainee"],
+
+  ["SG412","Mumbai","Hyderabad","B737 MAX 8","Passenger","01:30","Trainee"],
+  ["SG723","Hyderabad","Mumbai","B737 MAX 8","Passenger","01:30","Trainee"],
+
+  ["SG528","Kolkata","Hyderabad","B737 MAX 8","Passenger","02:20","Trainee"],
+  ["SG814","Hyderabad","Kolkata","B737 MAX 8","Passenger","02:20","Trainee"],
+
+  ["SG638","Bangalore","Hyderabad","Q400","Passenger","01:05","Cadet"],
+  ["SG903","Hyderabad","Bangalore","Q400","Passenger","01:05","Cadet"],
+
+  ["SG551","Chennai","Hyderabad","Q400","Passenger","01:00","Cadet"],
+  ["SG817","Hyderabad","Chennai","Q400","Passenger","01:00","Cadet"],
+
+  ["SG286","Delhi","Goa","B737 MAX 8","Passenger","02:35","Trainee"],
+  ["SG843","Goa","Delhi","B737 MAX 8","Passenger","02:35","Trainee"],
+
+  ["SG742","Kolkata","Goa","B737-900ER","Passenger","03:05","Captain"],
+  ["SG921","Goa","Kolkata","B737-900ER","Passenger","03:05","Captain"],
+
+  ["SG357","Pune","Delhi","B737 MAX 8","Passenger","02:00","Trainee"],
+  ["SG905","Delhi","Pune","B737 MAX 8","Passenger","02:00","Trainee"],
+
+  ["SG601","Ahmedabad","Mumbai","Q400","Passenger","01:10","Cadet"],
+  ["SG824","Mumbai","Ahmedabad","Q400","Passenger","01:10","Cadet"],
+
+  ["SG433","Jaipur","Delhi","Q400","Passenger","00:55","Cadet"],
+  ["SG812","Delhi","Jaipur","Q400","Passenger","00:55","Cadet"],
+
+  ["SG562","Lucknow","Delhi","B737 MAX 8","Passenger","01:20","Trainee"],
+  ["SG896","Delhi","Lucknow","B737 MAX 8","Passenger","01:20","Trainee"],
+
+  ["SG384","Bangalore","Kochi","Q400","Passenger","01:10","Cadet"],
+  ["SG911","Kochi","Bangalore","Q400","Passenger","01:10","Cadet"],
+
+  ["SG471","Chennai","Kochi","Q400","Passenger","01:00","Cadet"],
+  ["SG826","Kochi","Chennai","Q400","Passenger","01:00","Cadet"],
+
+  ["SG694","Delhi","Srinagar","B737 MAX 8","Passenger","01:25","Trainee"],
+  ["SG928","Srinagar","Delhi","B737 MAX 8","Passenger","01:25","Trainee"],
+
+  ["SG361","Mumbai","Srinagar","B737 MAX 8","Passenger","02:25","Trainee"],
+  ["SG877","Srinagar","Mumbai","B737 MAX 8","Passenger","02:25","Trainee"],
+
+  ["SG588","Delhi","Guwahati","B737 MAX 8","Passenger","02:30","Trainee"],
+  ["SG933","Guwahati","Delhi","B737 MAX 8","Passenger","02:30","Trainee"],
+
+  ["SG422","Kolkata","Guwahati","Q400","Passenger","01:15","Cadet"],
+  ["SG819","Guwahati","Kolkata","Q400","Passenger","01:15","Cadet"],
+
+  ["SG613","Bangalore","Goa","B737 MAX 8","Passenger","01:15","Trainee"],
+  ["SG945","Goa","Bangalore","B737 MAX 8","Passenger","01:15","Trainee"],
+
+  ["SG458","Chennai","Port Blair","B737 MAX 8","Passenger","02:25","Trainee"],
+  ["SG852","Port Blair","Chennai","B737 MAX 8","Passenger","02:25","Trainee"],
+
+  ["SG376","Mumbai","Kathmandu","B737 MAX 8","Passenger","02:45","Trainee"],
+  ["SG932","Kathmandu","Mumbai","B737 MAX 8","Passenger","02:45","Trainee"],
+
+  ["SG547","Delhi","Singapore","B737-900ER","Passenger","05:35","Captain"],
+  ["SG884","Singapore","Delhi","B737-900ER","Passenger","05:35","Captain"],
+
+  ["SG624","Mumbai","Singapore","B737-900ER","Passenger","05:10","Captain"],
+  ["SG918","Singapore","Mumbai","B737-900ER","Passenger","05:10","Captain"],
+
+  ["SG437","Bangalore","Singapore","B737 MAX 8","Passenger","04:45","Trainee"],
+  ["SG835","Singapore","Bangalore","B737 MAX 8","Passenger","04:45","Trainee"],
+
+  ["SG573","Chennai","Singapore","B737 MAX 8","Passenger","04:20","Trainee"],
+  ["SG947","Singapore","Chennai","B737 MAX 8","Passenger","04:20","Trainee"],
+
+  ["SG689","Delhi","Bangkok","B737-900ER","Passenger","04:20","Captain"],
+  ["SG954","Bangkok","Delhi","B737-900ER","Passenger","04:20","Captain"],
+
+  ["SG365","Kolkata","Bangkok","B737 MAX 8","Passenger","02:40","Trainee"],
+  ["SG823","Bangkok","Kolkata","B737 MAX 8","Passenger","02:40","Trainee"],
+
+  ["SG492","Mumbai","Muscat","B737 MAX 8","Passenger","02:55","Trainee"],
+  ["SG908","Muscat","Mumbai","B737 MAX 8","Passenger","02:55","Trainee"],
+
+  ["SG618","Delhi","Muscat","B737 MAX 8","Passenger","03:10","Trainee"],
+  ["SG936","Muscat","Delhi","B737 MAX 8","Passenger","03:10","Trainee"],
+
+  ["SG704","Bangalore","Sharjah","B737 MAX 8","Passenger","04:00","Trainee"],
+  ["SG951","Sharjah","Bangalore","B737 MAX 8","Passenger","04:00","Trainee"],
+
+  ["SG517","Chennai","Abu Dhabi","B737 MAX 8","Passenger","04:35","Trainee"],
+  ["SG927","Abu Dhabi","Chennai","B737 MAX 8","Passenger","04:35","Trainee"],
+
+  ["SG571","Ahmedabad","Kolkata","B737 MAX 8","Passenger","01:45","Trainee"],
+  ["SG572","Kolkata","Ahmedabad","B737 MAX 8","Passenger","01:45","Trainee"],
+
+  ["SG991","Delhi","Dharmshala","Q400","Passenger","01:00","Trainee"],
+  ["SG992","Dharmshala","Delhi","Q400","Passenger","01:00","Trainee"],
+
+  ["SG569","Lucknow","Dharmshala","Q400","Passenger","01:15","Trainee"],
+  ["SG570","Dharmshala","Lucknow","Q400","Passenger","01:15","Trainee"],
+
+  ["SG388","Delhi","Darbhanga","B737 MAX 8","Passenger","01:30","Trainee"],
+  ["SG389","Darbhanga","Delhi","B737 MAX 8","Passenger","01:30","Trainee"],
+
+  ["SG399","Mumbai","Darbhanga","B737 MAX 8","Passenger","02:00","Trainee"],
+  ["SG400","Darbhanga","Mumbai","B737 MAX 8","Passenger","02:00","Trainee"],
+
+  ["SG879","Delhi","Shillong","Q400","Passenger","03:00","Trainee"],
+  ["SG880","Shillong","Delhi","Q400","Passenger","03:00","Trainee"],
+
+  ["SG884","Delhi","Siliguri","B737 MAX 8","Passenger","02:00","Trainee"],
+  ["SG885","Siliguri","Delhi","B737 MAX 8","Passenger","02:00","Trainee"],
+
+  ["SG777","Delhi","Srinagar","B737 MAX 8","Passenger","01:00","Trainee"],
+  ["SG778","Srinagar","Delhi","B737 MAX 8","Passenger","01:00","Trainee"],
+
+  ["SG753","Mumbai","Varanasi","B737-900 ER","Passenger","01:45","Trainee"],
+  ["SG754","Varanasi","Mumbai","B737-900 ER","Passenger","01:45","Trainee"]
+
+];
+
+
+/*
+  CREATE ROUTE TABLE
+*/
+
+const tbody = document.querySelector("#routesTable tbody");
+
+tbody.innerHTML = routes.map(route => {
+
+  return `
+    <tr>
+
+      <td>${route[0]}</td>
+
+      <td>${route[1]}</td>
+
+      <td>${route[2]}</td>
+
+      <td>${route[3]}</td>
+
+      <td>${route[4]}</td>
+
+      <td>${route[5]}</td>
+
+      <td>${route[6]}</td>
+
+    </tr>
+  `;
+
+}).join("");
+
+
+
+/*
+  SIDEBAR
+*/
+
+const sidebar = document.getElementById("sidebar");
+
+const overlay = document.getElementById("overlay");
+
+const menuBtn = document.getElementById("menuBtn");
+
+const closeBtn = document.getElementById("closeBtn");
+
+const navItems = document.querySelectorAll(".nav-item");
+
+const sections = document.querySelectorAll(".page-section");
+
+
+
+function openMenu() {
+
+  sidebar.classList.add("open");
+
+  overlay.classList.add("show");
+
+}
+
+
+function closeMenu() {
+
+  sidebar.classList.remove("open");
+
+  overlay.classList.remove("show");
+
+}
+
+
+
+function showSection(id) {
+
+  sections.forEach(section => {
+
+    section.classList.toggle(
+      "active",
+      section.id === id
+    );
+
+  });
+
+
+  navItems.forEach(item => {
+
+    item.classList.toggle(
+      "active",
+      item.dataset.section === id
+    );
+
+  });
+
+
+  closeMenu();
+
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+
+}
+
+
+
+/*
+  EVENTS
+*/
+
+menuBtn.addEventListener(
+  "click",
+  openMenu
+);
+
+
+closeBtn.addEventListener(
+  "click",
+  closeMenu
+);
+
+
+overlay.addEventListener(
+  "click",
+  closeMenu
+);
+
+
+navItems.forEach(item => {
+
+  item.addEventListener(
+    "click",
+    () => showSection(item.dataset.section)
+  );
+
+});
